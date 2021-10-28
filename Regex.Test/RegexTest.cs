53d1bcdcb0d39afc8065fd056ba3d94b530127fe
Fenna <Fennahornstra@gmail.com>
2021-10-28 15:56:02 +0200

    Add project files.

---
+++ Regex.Test/RegexTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using static Regex.logic.regextester;
+
+namespace Regex.Test
+{
+    public class RegexTest
+    {  
+
+
+        [Theory]
+        [InlineData("aa", "a", false)]
+        [InlineData("aa", "a*", true)]
+        [InlineData("ac", ".*", true)]
+        [InlineData("aab", "c*a*b", true)]
+        [InlineData("mississippi", "mis*is*p*.",false)]
+        [InlineData("aaa", "a*a",true)]
+        [InlineData("ab", ".*c",false)]
+        [InlineData("bbbba", ".*a*a",true)]
+        [InlineData("a", ".*..a",false)]
+        public void IsMatchShouldReturnTrueOnValidString(string a, string b, bool solution)
+        {
+            var result = new Solution().IsMatch(a, b);
+            Assert.Equal(solution, result);
+        }
+    }
+}
---
+++ Regex.logic/RegexTester.cs
@@ -0,0 +1,67 @@
+﻿namespace Regex.logic
+{
+    public class regextester
+    {
+        public class Solution
+        {
+            public bool IsMatch(string s, string p)
+            {
+                if (s.Length == 0 && p.Length == 0)
+                    return true;
+                if ((s.Length != 0 && p.Length == 0))
+                    return false;
+
+                if (p.StartsWith("."))
+                {
+                    if (p.StartsWith(".*"))
+                    {
+                        if (s.Length == 0)
+                        {
+                            if (p.Length == 2)
+                                return true;
+
+                            return IsMatch(s, p.Substring(2));
+                        }
+                        if (!IsMatch(s, p.Substring(2)))
+                            return IsMatch(s.Substring(1), p);
+                        return true;
+
+                    }
+                    else
+                    {
+                        if (s.Length == 0)
+                            return false;
+                        return IsMatch(s.Substring(1), p.Substring(1));
+                    }
+                }
+
+                if (p.Length >= 2 && p[1].ToString().Equals("*"))
+                {
+                    if (s.Length == 0)
+                    {
+                        if (p.Length == 2)
+                            return true;
+
+                        return IsMatch(s, p.Substring(2));
+                    }
+                    if (s[0] == p[0])
+                    {
+                        if (!IsMatch(s, p.Substring(2)))
+                            return IsMatch(s.Substring(1), p);
+
+                        return true;
+                    }
+                    if (s[0] != p[0])
+                        return IsMatch(s, p.Substring(2));
+
+                }
+
+                if (s.Length > 0 && s[0] == p[0])
+                    return IsMatch(s.Substring(1), p.Substring(1));
+
+                return false;
+
+            }
+        }
+    }
+}
